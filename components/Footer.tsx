7d7accb441989ab2449fa53ccce60aa9158fafb6
Pablo E. L. Targino <pabloeltdev@proton.com>
2024-07-08 22:00:40 -0300

    Inserted footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,14 @@
 export default function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer>
+      <span>Made by with</span>
+      <span>&nbsp;</span>
+      <span>❤️</span>
+      <span>&nbsp;</span>
+      <span>by</span>
+      <span>&nbsp;</span>
+      <a href="https://link.tr/pabloelt">@pabloelt</a>
+      <span></span>
+    </footer>
+  );
 }
