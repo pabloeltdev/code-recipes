da5458686e72c7f95ff9ddf3daed818fb2006856
Pablo E. L. Targino <pabloeltdev@proton.com>
2024-07-23 23:31:21 -0300

    Add url to recipe

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -8,8 +8,10 @@ export default function Recipes() {
   const recipe: Recipe = {
     title: "Composition",
     labels: ["React", "Design Pattern"],
+    url: "https://github.dev/pabloeltdev/code-recipes/blob/c5280dd157835109fed88d86c9b685477f49716d/components/ui/Card.tsx",
   };
   const recipes = Array(15).fill(recipe);
+
   return (
     <main>
       <div className={styles.searchBarContainer}>
